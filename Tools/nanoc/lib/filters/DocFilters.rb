68320bf993115765b7f32f55c3b268be4d6bb2c9
jln- <julien.rabin@gmail.com>
2010-11-30 10:42:07 +0100

    optimized a bit regexp in DocFilters

--- Tools/nanoc/lib/filters/DocFilters.rb
+++ Tools/nanoc/lib/filters/DocFilters.rb
@@ -3,6 +3,13 @@ identifier :addref
 
   def run(content, params={})
     content.gsub(/(\s)(jcom.\w+)/,' "(jcom)\2":./components/\2.html')
-    content.gsub(/(\s)(jmod.+)(\s)/,' "(jcom)\2":./components/\2.html ') # TODO make the regexp more error proof (i.e modules with dot in name)
+    content.gsub(/(\s)(jmod.[\w|\.]+)/,' "(jcom)\2":./components/\2.html')
+  end
+end
+
+class PreTextileFilter < Nanoc3::Filter
+  identifier :pretextile
+  
+  def run(content, params={})
   end
 end
