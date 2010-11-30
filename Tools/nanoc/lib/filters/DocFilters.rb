81b26f0a0e6fb3971396dc19dfc0a010dcf48262
jln- <julien.rabin@gmail.com>
2010-11-30 10:34:28 +0100

    DocFilter now also adds ref links for jmod.modules

--- Tools/nanoc/lib/filters/DocFilters.rb
+++ Tools/nanoc/lib/filters/DocFilters.rb
@@ -3,5 +3,6 @@ identifier :addref
 
   def run(content, params={})
     content.gsub(/(\s)(jcom.\w+)/,' "(jcom)\2":./components/\2.html')
+    content.gsub(/(\s)(jmod.+)(\s)/,' "(jcom)\2":./components/\2.html ') # TODO make the regexp more error proof (i.e modules with dot in name)
   end
 end
