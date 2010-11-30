613f3af4ab359115c7642db65b1a9aea8e592d45
jln- <julien.rabin@gmail.com>
2010-11-30 12:48:15 +0100

    added filter to wrap image tag in div for lightbox-like plugins

--- Tools/nanoc/lib/filters/DocFilters.rb
+++ Tools/nanoc/lib/filters/DocFilters.rb
@@ -9,8 +9,17 @@ end
 
 class PreTextileFilter < Nanoc3::Filter
   identifier :pretextile
-# TODO turn these rules in a class extenting Redcloth -> should be usable in non-nanoc generated html
+# TODO turn these rules in a class extenting Redcloth -> should be more easily usable in non-nanoc generated html
   def run(content, params={})
     content.gsub(/(\()(\d+)(\))/,'\1<span class="figurePointer">\2</span>\3')
+    content.gsub(/(!)(\S+)(!)/,'<div class="imageZoom"><a href="\2" class="image" target="_blank" rel="lightbox[modular]"><img src="\2"></a></div>')
   end
 end
+
+# class ZoomboxFilter < Nanoc3::Filter
+# identifier :zoomify
+#   def run(content, params={})
+#     nil
+#     # content.gsub(/(!)(\S+)(!)/,'<div class="imageZoom"><a href="\2" class="image" target="_blank" rel="lightbox[modular]"><img src="\2"></a></div>')
+#   end
+# end
