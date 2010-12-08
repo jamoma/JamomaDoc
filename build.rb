9792327327d4b91f744d5681534c2daf8dc050e5
jln- <julien.rabin@gmail.com>
2010-12-08 23:14:25 +0100

    maked build.rb script a bit more verbose

--- build.rb
+++ build.rb
@@ -5,11 +5,21 @@ glibdir = "."
 Dir.chdir glibdir
 glibdir = Dir.pwd
 
+puts "\n\ncleaning...\n\n"
+`rm -rd #{glibdir}/Tools/nanoc/content`
+`rm -rd #{glibdir}/build`
+
+puts "Copying Jamoma Documentation"
+puts "==================================================="
 `cp -R #{glibdir}/Modular/Text/ #{glibdir}/Tools/nanoc/content`
 `cp -R #{glibdir}/Tools/htmlLib/css #{glibdir}/Tools/nanoc/content`
 `cp -R #{glibdir}/Tools/htmlLib/js #{glibdir}/Tools/nanoc/content`
 
+puts "\n\nCompiling Jamoma Documentation using nanoc"
+puts "==================================================="
+
 Dir.chdir "#{glibdir}/Tools/nanoc"
 
-`nanoc co`
-
+nanocOutput = `nanoc compile`
+puts nanocOutput
+puts "=================DONE===================="
