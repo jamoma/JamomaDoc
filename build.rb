ea029c7c154c2a648729867e3478a64df0655494
+1m place <tim@74objects.com>
2011-01-27 09:22:36 -0600

    nanoc (or something else in this build script) throws errors with ruby 1.9.1

--- build.rb
+++ build.rb
@@ -1,4 +1,7 @@
-#!/usr/bin/env ruby -wKU
+#!/usr/bin/ruby -wKU
+# TAP: changed the above so that on the Mac it always uses the version of ruby installed by the OS
+# this is because version 1.8.7 works, but 1.9.1 does not
+#/usr/bin/env ruby -wKU
 
 #TODO redirect nanoc output to Terminal
 glibdir = "."
