3b9c3b980c386be5dabb49efb9a4b9e34c7cd0c7
@jln_ <julien.rabin@gmail.com>
2011-11-21 16:49:46 +0100

    some initial work on the build script. Mostly just useful for testing for now

--- build.rb
+++ build.rb
@@ -3,35 +3,20 @@
 # this is because version 1.8.7 works, but 1.9.1 does not
 #/usr/bin/env ruby -wKU
 
-#TODO redirect nanoc output to Terminal
 glibdir = "."
 Dir.chdir glibdir
 glibdir = Dir.pwd
 
-require 'rubygems'
+search = `find . -name *maxref.yml` #Does this only work on OSX ?
 
-if Gem.available?(/nanoc/)
-  
-puts "\n\ncleaning...\n\n"
-`rm -rd #{glibdir}/Tools/nanoc/content`
-`rm -rd #{glibdir}/build`
+puts search
 
-puts "Copying Jamoma Documentation"
-puts "==================================================="
-`cp -R #{glibdir}/Modular/Text/ #{glibdir}/Tools/nanoc/content`
-`cp -R #{glibdir}/Tools/htmlLib/css #{glibdir}/Tools/nanoc/content`
-`cp -R #{glibdir}/Tools/htmlLib/js #{glibdir}/Tools/nanoc/content`
+maxrefs = Array::new
+maxrefs = search.split("\n")
 
-puts "\n\nCompiling Jamoma Documentation using nanoc"
-puts "==================================================="
+maxrefs.each do |ref|
+ dest = ref.sub(/\.(.*jcom.*maxref)\.yml/,"#{glibdir}"'\1.xml')
+ ref = ref.sub(/\.(.*jcom.*maxref.yml)/,"#{glibdir}"'\1')
+`ruby #{glibdir}/yaml-to-maxref.rb #{ref} #{dest}`
 
-Dir.chdir "#{glibdir}/Tools/nanoc"
-
-nanocOutput = `nanoc compile`
-puts nanocOutput
-puts "=================DONE===================="
-
-else
-  puts "===================================================\nDocumentation cannot be compiled. The Ruby gem needed cannot be found.\n\nAssuming you have Rubygem installed, please install last version of nanoc running the following command:\n\n\'sudo gem install nanoc\'\n\nYou also may want to visit nanoc website to learn more about it.\n\nhttp://nanoc.stoneship.org/\n\n==================================================="
-  
 end
