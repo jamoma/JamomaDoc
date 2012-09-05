b3b9fb09a286adea1ce261a4d9a0223be7aa5e05
+1m place <tim@74objects.com>
2012-09-05 10:52:34 -0500

    API: starting to get narrative chapters pulled-in

---
+++ API/chapters/01-Introduction.h
@@ -0,0 +1,36 @@
+/**
+	@mainpage A non-exhaustive introduction to the Jamoma platform
+
+	The Jamoma Platform is composed of a layered framework architecture that creates an object model 
+	and then specializes that object model for advanced purposes such as audio and graphics.
+
+	- Foundation : Runtime and API implementing a light-weight, dynamically bound, reflective object-oriented architecture layer on top of C++
+	- DSP : A C++ framework for audio processing objects
+	- Graph : A coding layer for the creation of dynamic asynchronous graph topographies
+	- Audio Graph : A coding layer for the creation of dynamic audio graph topographies
+	- Graphics : A Cairo-based graphics system build on the Jamoma Foundation API
+	- Modular : A modular framework for patching in Max
+	- Ruby : Jamoma Ruby - Language Bindings for the Jamoma Platform
+	- Test : Automated unit test system for Jamoma
+	- Tools : Tools and Utilities for the Development and Distribution of Jamoma
+
+	TTBlue is a reflective...  (copy and paste from website).
+
+	Discuss the vocabulary/jargon term-by-term -- NOTE: This is done in the ICMC paper on the DSP lib.
+
+	Chapters:
+	- @ref chapter_datatypes
+	- @ref chapter_usingobjects
+	- @ref chapter_writingobjects
+	- @ref chapter_extendingobjects
+	- @ref chapter_unittesting
+	- @ref chapter_objectwrappers
+
+	Appendices:
+	- @ref appendix_acknowledgements
+	- @ref appendix_tags
+	- @ref appendix_styleguide
+
+
+*/
+
