1d1fe8a881f858a47c710301134152f670fd96a3
Nathan Wolek <nw@nathanwolek.com>
2012-09-12 15:51:05 +0200

    clean up formatting narrative 03

--- API/chapters/03-UsingObjects.h
+++ API/chapters/03-UsingObjects.h
@@ -1,19 +1,19 @@
 /** 
 	@page chapter_usingobjects How to use objects from the Jamoma API
 
-	h2. Object Life Cycle
+	@section chapter_usingobjects_lifecycle Object Life Cycle
 
-	h3. Creating and Destroying
+	@subsection chapter_usingobjects_lifecycle_createdestroy Creating and Destroying
 
 	To create an instance of a Jamoma object, use TTObjectInstantiate().  
 	This will look up the class in Jamoma’s registry of objects and return a pointer to the new instance. 
  
 	An example below creates a stereo instance of an allpass filter, and two stereo audio signals.
 	They are stereo because the argument given when instantiating these types of objects define the initial number of audio channels as two.
-	You can safely ignore the fact that one of the variables is a TTAudioObjectPtr instead of a \!{TTObjectPtr}.  
-	TTAudioObject is simply a specialized version of #TTObject.
+	You can safely ignore the fact that one of the variables is a TTAudioObjectPtr instead of a TTObjectPtr.  
+	TTAudioObject is simply a specialized version of TTObject.
 
-	<pre class="cpp">
+	@code
 		TTAudioObjectPtr  myObject      = NULL;
 		TTObjectPtr       myAudioInput  = NULL;
 		TTObjectPtr       myAudioOutput = NULL;
@@ -21,23 +21,23 @@
 		TTObjectInstantiate(TT("allpass")), &myObject, 2);
 		TTObjectInstantiate(TT("audiosignal")), &myAudioInput, 2);
 		TTObjectInstantiate(TT("audiosignal")), &myAudioOutput, 2);
-	</pre>
+	@endcode
 
 	When you are all done using the objects, you need to release them.  
 	You can do this with the TTObjectRelease() function.  
 	For example:
 
-	<pre class="cpp">
+	@code
 		TTObjectRelease(&myObject);
 		TTObjectRelease(&myAudioInput);
 		TTObjectRelease(&myAudioOutput);
-	</pre>
+	@endcode
 
-	h3. Retaining and Reference Counts
+	@subsection chapter_usingobjects_lifecycle_referencecounts Retaining and Reference Counts
 
 	TTObject’s are reference counted.  This means that you can create references to existing objects, and the object will not be freed until all references have been released.  This is demonstrated in the following example:
 
-	<pre class="cpp">
+	@code
 		TTObjectPtr myObject = NULL;
 		TTObjectPtr aReferenceToMyObject = NULL;
 
@@ -50,17 +50,17 @@
 		// The object is not actually deleted, but the reference count is now 1
 		TTObjectRelease(&aReferenceToMyObject);
 		// Now, because the reference count fell to zero, the object is deleted.
-	</pre>
+	@endcode
 
-	h2. Querying the Environment
+	@section chapter_usingobjects_queryenvironment Querying the Environment
 
 	As alluded to in section 3.1, Jamoma Foundation maintains a registry of available classes that may be instantiated.  Some of these classes are implemented internally in the Jamoma libraries, and some are implemented externally as Jamoma Extensions.  The registry does not differentiate between these, but simply provides a list of everything that is loaded in the system.
 
-	h3. Getting All Class Names
+	@subsection chapter_usingobjects_queryenvironment_getallclassnames Getting All Class Names
 
 	To obtain a list of all class in the Foundation's registry, you call the TTGetRegisteredClassNames() function as in the following example:
 
-	<pre class="cpp">
+	@code
 		TTErr err;
 		TTValue classNames;
 
@@ -79,15 +79,15 @@
 				TTLogMessage("class name: %s", aClassName->getCString());
 			}
 		}
-	</pre>
+	@endcode
 
-	h3. Searching For Classes Based on Tags
+	@subsection chapter_usingobjects_queryenvironment_searchbasedontags Searching For Classes Based on Tags
 
 	In addition to retrieving all class names, it is also useful to be able to retrieve a limited number of class names based on criteria that you specify.  For example, you may wish to only list classes that generate their own audio.  Or perhaps only those class which implement some sort of lowpass filter.
 
 	In this case we call TTGetRegisteredClassNamesForTags(), and process the results in the same manner as we did for TTGetRegisteredClassNames().  This is demonstrated in the example below:
 
-	<pre class="cpp">
+	@code
 		TTErr err;
 		TTValue classNames;
 		TTValue searchTags;
@@ -112,15 +112,15 @@
 				TTLogMessage("class name: %s", aClassName->getCString());
 			}
 		}
-	</pre>
+	@endcode
 
 	For a list of common tags and what they mean in Jamoma see Appendix B.  To get a list of all tags in use at any time, call TTGetRegisteredTags().
 
-	h4. Tag Searching and Instantiation in Action
+	@subsubsection chapter_usingobjects_queryenvironment_searchbasedontags_example Tag Searching and Instantiation in Action
 
 	Here is an example that searches based on tags for a lowpass filter using a Butterworth algorithm, and then creates an instance of that class.
 
-	<pre class="cpp">
+	@code
 		// In this case there are multiple matches returned. 
 		// Since more specific information was not provided, 
 		// we just instantiate the first one.
@@ -142,25 +142,26 @@
 			err = TTObjectInstantiate(classNames, &butterworthFilter, 1);
 		}
 		// now do something with the filter.
-	</pre>
+	@endcode
 
-	h2. Sending Messages
+	@section chapter_usingobjects_sendingmessages Sending Messages
 
 	Having created an instance of an object, we must now do something with that object.
 	We do things with objects by sending them messages. 
 	A message defines an action to be performed. 
 
-	Given the Butterworth filter example in \ref{Tag Searching and Instantiation in Action}, we can now send the filter the 'clear' message to zero its sample history\footnote{The Butterworth filter is an IIR filter, meaning that it stores the results of previous calculations to perform future calculations.  This feedback can sometimes get out of control, and thus the necessity for a 'clear' message.}.
+	Given the Butterworth filter example in @ref chapter_usingobjects_queryenvironment_searchbasedontags_example, we can now send the filter the 'clear' message to zero its sample history
+	@note The Butterworth filter is an IIR filter, meaning that it stores the results of previous calculations to perform future calculations.  This feedback can sometimes get out of control, and thus the necessity for a 'clear' message.
 
-	<pre class="cpp">
+	@code
 		butterworthFilter->sendMessage(TT("clear"));
-	</pre>
+	@endcode
 
 	Some messages, like the 'clear' message for the Butterworth filter, require no additional information to perform the requested action.
 	Other messages, however, do require additional information.  
 	This information can be provided using an optional argument to the sendMessage method.  Here are some samples:
 
-	<pre class="cpp">
+	@code
 		someObject->sendMessage(TT("foo"), 3.14);
 		someObject->sendMessage(TT("draw"), TT("circle"));
 
@@ -185,20 +186,20 @@
 			// Probably the object you sent the message to knows this message and is returning 
 			// an error specific to the action you requested.
 		}
-	</pre>
+	@endcode
 
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-	h2. Setting and Getting Attributes
+	@section chapter_usingobjects_setgetattributes Setting and Getting Attributes
 
 	Sending messages is great for performing actions, however these actions do not represent the state of the object.
 	The state of the object is represented as the object's 'attributes'.
 
-	h2. Querying Objects for Available Messages and Attributes
+	@section chapter_usingobjects_queryavailable Querying Objects for Available Messages and Attributes
 
-	h2. Processing Audio
+	@section chapter_usingobjects_processingaudios Processing Audio
 
-	Not all objects can process audio, however, objects that have the tag ‘audio’ associated with them are able process audio.  These objects derive from TTAudioObject, a subclass of #TTObject which implements the “process” method.
+	Not all objects can process audio, however, objects that have the tag ‘audio’ associated with them are able process audio.  These objects derive from #TTAudioObject, a subclass of #TTObject which implements the “process” method.
 
 */
 
